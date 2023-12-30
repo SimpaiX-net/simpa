b8cc811796af252571cb970364002bfba5c59b13
z3ntl3 <z3nt3discord@gmail.com>
2023-12-30 10:21:57 +0100

    12/30/2023 10:21:57

--- engine/sessions/sess.go
+++ engine/sessions/sess.go
@@ -1,3 +1,4 @@
+// TODO
 package sessions
 
 import "net/http"
@@ -11,10 +12,21 @@ type Session struct {
 }
 
 type SessionI interface {
+	// Opts should contain cookie options, minimum required field is
+	// [opts.Name]
+	//
+	// Loads session from datastore and syncs with client session cookie
+	// when one is present or creates new one.
+	//
+	// For authentication and integrity auth it uses ``app.SecureCookie`` crypter.
+	// Default crypters are AES CTR HMAC or AES GCM HMAC
 	New(r *http.Request, opts http.Cookie) (*Session, error)
 
 	// Saves the session to the database.
-	// It also rewrites and sends it to the client
+	// It also overwrites client cookie
+	//
+	// The client cookie holds cookie.Name = sessionName
+	// cookie.Value = [encrypted value] -> using ``app.SecureCookie``
 	Save() error
 
 	// Gets session value [key]
@@ -26,18 +38,34 @@ type SessionI interface {
 	// Deletes session value [key]=val
 	DeleteVal(key string) error
 
+	// Deletes all session values
+	DeleteValAll() error
+
 	// Destroys the session both from database and client
 	Destroy() error
 }
 
 // Store implementation for sessions.
-// Uses app.SecureCookie crypter to crypt. Standard crypters use AES_CTR or AES_GCM.
+// Uses app.SecureCookie crypter to crypt. Standard crypters use AES_CTR HMACor AES_GCM HMAC.
 type SessionStore interface {
-	New(id string) *Session
+	// Loads session from the session cookie sent form the client or
+	// generates a new one.
+	//
+	// When loading, the client cookie is authenticated and then loaded from data store.
+	// For setting any cookies Simpa does use ``engine.SecureCookie``.
+	// ``engine.SecureCookie`` contains a crypter model.
+	//
+	// Defaults to AES GCM HMAC or AES CTR HMAC crypter near choice.
+	//
+	// To summarize when loading cookies its authenticity and integrity
+	// is authenticated first before trying to load.
+	//
+	// Otherwise it creates a new session
+	New(name string) *Session
 
 	Save(session *Session) error
 
-	Purge() error
+	Purge(id string) error
 
-	Get() *Session
+	Get(key string) interface{}
 }
