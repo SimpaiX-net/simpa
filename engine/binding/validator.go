752505bca4c2ba2552c7b641d6bd2891c4856261
z3ntl3 <z3nt3discord@gmail.com>
2024-02-12 22:35:48 +0100

    02/12/2024 22:35:48

--- engine/binding/validator.go
+++ engine/binding/validator.go
@@ -41,7 +41,7 @@ Validates the given struct with the custom engine underlying in v
 func (v *Validator) ValidateStruct(data interface{}) error {
 	var err error
 
-	err = v.ValidateStruct(data)
+	err = v.engine.Struct(data)
 	if err != nil {
 		if _, ok := err.(*validator.InvalidValidationError); ok {
 			return errors.New("InvalidValidationError describes an invalid argument passed to `Struct`, `StructExcept`, StructPartial` or `Field`")
